f047e2b8cc3f4de4f7437bd4d76fe50a84de9dcd
Gabriel Njoabozia <mails4gabriel@gmail.com>
2024-12-06 18:07:06 +0100

    updated favicon

--- src/app/_fragments/navbar.tsx
+++ src/app/_fragments/navbar.tsx
@@ -13,67 +13,74 @@ export default function Navbar() {
 
   const toggleHamburger = () => setShow(!show);
   return (
-    <Wrapper>
+    <Wrapper className="mb-[5.5rem]">
       <section id="home">
         {/* DESKTOP NAVIGATION */}
-        <nav className="h-[5.5rem] flex justify-between items-center relative">
-          <Link href="/#home">
-            <Image src={LOGO} alt="boomcard logo" width={180} height={40} />
-          </Link>
-          <ul role="menubar" className="hidden lg:flex items-center">
-            {NAV_ITEMS.map((item, i) => (
-              <li
-                key={i}
-                role="none"
-                className={cn("px-10 last:pe-0", item?.style)}
-              >
-                {item?.type === "text" && (
-                  <Link href={item.route} className="font-medium">
-                    {item?.title}
-                  </Link>
-                )}
-                {item?.type === "button" && (
-                  <Button variant="onboarding" className="h-[48px] w-[144px]">
-                    {item?.title}
-                  </Button>
-                )}
-              </li>
-            ))}
-          </ul>
-          <span className="lg:hidden" onClick={toggleHamburger}>
-            <HamburgerButton isOpen={show} toggleHamburger={toggleHamburger} />
-          </span>
-        </nav>
-        {/* MOBILE NAVIGATION */}
-        {show && (
-          <Wrapper className="lg:hidden absolute left-0 right-0 w-100%">
-            <ul role="menubar" className="bg-[#F4F4F0]">
-              {MOBILE_NAV_ITEMS.map((item, i) => (
-                <li key={i} role="none" className="w-full py-4 last:pt-0">
+        <nav className="fixed top-0 left-0 w-full z-50 bg-[#F4F4F0]">
+          <Wrapper className="flex justify-between items-center h-[5.5rem]">
+            <Link href="/#home">
+              <Image src={LOGO} alt="boomcard logo" width={180} height={40} />
+            </Link>
+            <ul role="menubar" className="hidden lg:flex items-center">
+              {NAV_ITEMS.map((item, i) => (
+                <li
+                  key={i}
+                  role="none"
+                  className={cn("px-10 last:pe-0", item?.style)}
+                >
                   {item?.type === "text" && (
-                    <Link
-                      href={item.route}
-                      className="font-medium"
-                      onClick={toggleHamburger}
-                    >
+                    <Link href={item.route} className="font-medium">
                       {item?.title}
                     </Link>
                   )}
                   {item?.type === "button" && (
-                    <Button
-                      className="h-[48px] w-full"
-                      variant={
-                        item.title === "Get Started" ? "onboarding" : "ghost"
-                      }
-                      onClick={toggleHamburger}
-                    >
+                    <Button variant="onboarding" className="h-[48px] w-[144px]">
                       {item?.title}
                     </Button>
                   )}
                 </li>
               ))}
             </ul>
+            <span className="lg:hidden" onClick={toggleHamburger}>
+              <HamburgerButton
+                isOpen={show}
+                toggleHamburger={toggleHamburger}
+              />
+            </span>
           </Wrapper>
+        </nav>
+        {/* MOBILE NAVIGATION */}
+        {show && (
+          <nav className="lg:hidden fixed left-0 right-0 bg-[#F4F4F0] w-full top-[5.5rem] z-40">
+            <Wrapper className="">
+              <ul role="menubar">
+                {MOBILE_NAV_ITEMS.map((item, i) => (
+                  <li key={i} role="none" className="w-full py-4 last:pt-0">
+                    {item?.type === "text" && (
+                      <Link
+                        href={item.route}
+                        className="font-medium"
+                        onClick={toggleHamburger}
+                      >
+                        {item?.title}
+                      </Link>
+                    )}
+                    {item?.type === "button" && (
+                      <Button
+                        className="h-[48px] w-full"
+                        variant={
+                          item.title === "Get Started" ? "onboarding" : "ghost"
+                        }
+                        onClick={toggleHamburger}
+                      >
+                        {item?.title}
+                      </Button>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </Wrapper>
+          </nav>
         )}
       </section>
     </Wrapper>
